a915611e216f2099a9fd00e065c0dc32e1a2d495
test <test@tests-MacBook-Pro.local>
2017-09-15 22:28:46 +0200

    add cmd argument for server_addr

--- zeromg/zmq_client.py
+++ zeromg/zmq_client.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #
 #   Hello World client in Python
 #   Connects REQ socket to tcp://localhost:5555
@@ -5,13 +6,18 @@
 #
 
 import zmq
+import argparse
 
 context = zmq.Context()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--server_addr", required=False, default='localhost', help="address to server")
+args = parser.parse_args()
 
 #  Socket to talk to server
 print("Connecting to hello world server...")
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+socket.connect("tcp://%s:5555" % args.server_addr)
 
 #  Do 10 requests, waiting each time for a response
 for request in range(10):
