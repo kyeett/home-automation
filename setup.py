93e302057debaa7be229d9513b304af9ab683435
test <test@tests-MacBook-Pro.local>
2017-09-17 20:10:26 +0200

    add setup.py for protobuf_examples

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='protobuf_decoder',
+      version='0.9',
+      description='Python Protobuf Decoder',
+      author='Magnus Wahlstrand',
+      author_email='magnus.wahlstrand@gmail.com',
+      url='https://www.python.org/',
+      packages=['protobuf_examples'],
+     )
